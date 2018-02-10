9d8fe72dab6437e88f2211c6cee23ac45239cc19
LukeLinEx <lukelinex@gmail.com>
2018-02-09 19:17:21 -0500

    to pull

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='aerotropolis_app',
+    packages=["aerotropolis_app"],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'flask_wtf'
+        'pymongo',
+        'boto3'
+    ],
+)
